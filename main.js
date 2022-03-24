f31347369856f48a12c42a25137a7d865bebf722
Michael <99843466+MichaelQP@users.noreply.github.com>
2022-03-23 19:40:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+function game() {
+   playRound();
+}
+function playRound() {
+    const playSelection = playerChoice();
+    const computerSelection = computerChoice();
+}
+//added a comment for github testing
+function playerChoice() {
+    let input = prompt('Type rock, paper, or scissors');
+    while(input == null){
+        input = prompt('Type rock, paper, or scissors');
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    if (check == true) {
+        console.log(input)
+    }
+    
+    }
+    console.log(input);
+}
+
+function computerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function validateInput(choice){
+    if(choices.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+game();
